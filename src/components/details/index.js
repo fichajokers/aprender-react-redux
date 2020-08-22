597c3dcdbe256f8b35b7a430ff350b0c4c5e3985
matias.villar <matias.villar@uv.cl>
2020-08-22 16:18:49 -0400

    Crear componetnes con el patron de diseño contenedor y componente presentacional, ademas de utiliar react-router-dom

---
+++ src/components/details/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Page from './page'
+
+export default class Details extends Component {
+    render() {
+        return (
+            <Page />
+        )
+    }
+}
---
+++ src/components/results/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Page from './page'
+
+export default class Results extends Component {
+    render() {
+        return (
+            <Page />
+        )
+    }
+}
